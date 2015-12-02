6a8ebe7a4dd4549c134ecc286d7f7198aecaa9f2
Tim James <timbjames@gmail.com>
2015-12-02 11:52:11 +0000

    added bower config, react dependency, typescript structure and configured typescript

---
+++ src/VNextStacks/Scripts/app.tsx
@@ -0,0 +1,18 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+
+}
+
+class MyApp extends React.Component<IAppProps, IAppState> {
+    constructor(props: any) {
+        super(props);
+    }
+}
---
+++ src/VNextStacks/wwwroot/js/mylibs/app.js
@@ -0,0 +1,16 @@
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var MyApp = (function (_super) {
+    __extends(MyApp, _super);
+    function MyApp(props) {
+        _super.call(this, props);
+    }
+    return MyApp;
+})(React.Component);
+//# sourceMappingURL=app.js.map
