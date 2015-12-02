3c6f8b1b1518901f3cbc274916e7ea0d0aa6e833
Tim James <timbjames@gmail.com>
2015-12-02 14:35:07 +0000

    added updated react type definitions. updated to use reactdom

--- src/VNextStacks/Scripts/app.tsx
+++ src/VNextStacks/Scripts/app.tsx
@@ -1,7 +1,9 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+﻿/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/react/react-global.d.ts" />
+
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-
 
 interface IAppProps {
 
@@ -12,7 +14,18 @@ interface IAppState {
 }
 
 class MyApp extends React.Component<IAppProps, IAppState> {
+
     constructor(props: any) {
         super(props);
     }
+
+    render() {
+        return (<h1>Hello World</h1>);
+    }
+
 }
+
+$(() => {
+    var $mountNode = $('#appMountNode');     
+    ReactDOM.render(<MyApp />, $mountNode[0]);
+});
--- src/VNextStacks/wwwroot/js/mylibs/app.js
+++ src/VNextStacks/wwwroot/js/mylibs/app.js
@@ -1,6 +1,5 @@
-// A '.tsx' file enables JSX support in the TypeScript compiler, 
-// for more information see the following page on the TypeScript wiki:
-// https://github.com/Microsoft/TypeScript/wiki/JSX
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/react/react-global.d.ts" />
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -11,6 +10,13 @@ var MyApp = (function (_super) {
     function MyApp(props) {
         _super.call(this, props);
     }
+    MyApp.prototype.render = function () {
+        return (React.createElement("h1", null, "Hello World"));
+    };
     return MyApp;
 })(React.Component);
+$(function () {
+    var $mountNode = $('#appMountNode');
+    ReactDOM.render(React.createElement(MyApp, null), $mountNode[0]);
+});
 //# sourceMappingURL=app.js.map
